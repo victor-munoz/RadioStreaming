2f0c3db194f2067b51a70e0b8a0b850a35ebee38
victor-munoz <vmunozisler@gmail.com>
2019-09-09 22:29:23 +0200

    return MediaSource instead of ProgressiveMediaSource

--- app/src/main/java/victor/digitalmediahub/task/dependencyInjection/modules/ExoPlayerModule.kt
+++ app/src/main/java/victor/digitalmediahub/task/dependencyInjection/modules/ExoPlayerModule.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.Uri
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
@@ -22,11 +23,11 @@ class ExoPlayerModule {
         return DefaultDataSourceFactory(context, Util.getUserAgent(context, context.resources.getString(R.string.app_name)))
     }
     @Provides
-    internal fun providesProgressiveMediaSource(dataSourceFactory:DefaultDataSourceFactory): ProgressiveMediaSource {
+    internal fun providesProgressiveMediaSource(dataSourceFactory:DefaultDataSourceFactory): MediaSource {
         return ProgressiveMediaSource.Factory(dataSourceFactory) .createMediaSource(Uri.parse(RADIO_URL))
     }
     @Provides
-    internal fun providesSimpleExoPlayer(context: Context, mediaSource: ProgressiveMediaSource): SimpleExoPlayer{
+    internal fun providesSimpleExoPlayer(context: Context, mediaSource: MediaSource): SimpleExoPlayer{
         val player = ExoPlayerFactory.newSimpleInstance(context)
         player.prepare(mediaSource)
         return player
